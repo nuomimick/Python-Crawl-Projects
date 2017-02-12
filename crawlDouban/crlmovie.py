136228e2d67d0f87c2865ef0d9258f43268f789c
nuomimick <nuomimick@163.com>
2017-02-12 12:54:07 +0800

    可以运行的

--- crawlDouban/crlmovie.py
+++ crawlDouban/crlmovie.py
@@ -19,16 +19,17 @@ class crawlMovies():
 				if not thd.isAlive():
 					threads.remove(thd)
 			if len(threads) < max_threads and crawl_queue:#任务列表不为空且线程数小于最大线程数，新建线程
-				thread = threading.Thread(target=process_url,args=(crawl_queue,seen))
+				thread = threading.Thread(target=self.process_url,args=(crawl_queue,seen,link_regex))
 				thread.setDaemon(True)
 				thread.start()
 				threads.append(thread)
 			#等待(线程池满了或者任务队列为空)
 			time.sleep(SLEEP_TIME)
 			
-	def process_url(self,crawl_queue,seen):
+	def process_url(self,crawl_queue,seen,link_regex):
 		url = crawl_queue.pop()
 		html = self.download(url)
+		self.crl_content(html)
 		for link in self.get_links(html):
 			if re.match(link_regex,link):
 				if link not in seen:
@@ -40,15 +41,16 @@ class crawlMovies():
 		html = etree.HTML(rsp.text)
 		return html
 
-	def crl_content(self):
-		pass
+	def crl_content(self,html):
+		title = html.xpath('//title/text()')
+		print(title)
 
 	def get_links(self,html):
 		rst = html.xpath('//a/@href')
 		return set(rst)
 
 if __name__ == '__main__':
-	# seed_url = 'https://movie.douban.com/subject/26635319/'
-	# pattern = re.compile(r'https://movie.douban.com/subject/\d+/\?from.*')
-	# cm = crawlMovies()
-	# html = cm.crawler(seed_url,pattern)
+	seed_url = 'https://movie.douban.com/subject/26635319/'
+	pattern = re.compile(r'https://movie.douban.com/subject/\d+/\?from.*')
+	cm = crawlMovies()
+	cm.crawler(seed_url,pattern)
