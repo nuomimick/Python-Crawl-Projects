c832ee8176ae51bbfc5d4d3bf4ef1be4f011c328
nuomimick <350478064@qq.com>
2017-02-21 18:46:01 +0800

    修改

--- crawlDouban/crlcomments.py
+++ crawlDouban/crlcomments.py
@@ -10,26 +10,17 @@ from multiprocessing.dummy import Pool
 
 class CrlComments:
     def __init__(self):
-        self.loop = asyncio.get_event_loop()
-
-        login_url = 'https://accounts.douban.com/login'
-        self.session = requests.Session()
-        self.session.headers = {
-            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, sdch, br",
-            "Accept-Language": "zh-CN, zh; q=0.8, en; q=0.6",
-        }
+        pass
         #模拟登录
+        # login_url = 'https://accounts.douban.com/login'
         # data = {'form_email':b64decode(b'MzUwNDc4MDY0QHFxLmNvbQ=='),'form_password':b64decode(b'emhvdTkzMDcyMQ=='),\
         # 'source':'movie','login':'登录','redir':'https://movie.douban.com'}
         # r = self.session.post(login_url,data=data)
         # print(r.status_code,'login successfully!')
         # self.session.cookies.update(r.cookies)
 
-    def get_comments(self, response):
-        url = response.url
-        html = etree.HTML(response.text)
+    def get_comments(self, text):
+        html = etree.HTML(text)
         divs = html.xpath('//div[@class="comment-item"]')
         if len(divs) > 1:
             for div in divs:
@@ -45,36 +36,43 @@ class CrlComments:
                     ['url','user_name','user_url','saw','rating','time','content'])
                 df.to_csv('comments.csv',mode='a',index=False,encoding='utf-8',header=False)
 
+    #不使用aiohttp
     # async def download(self,url,*args,**kwargs):
     #     future = self.loop.run_in_executor(None,partial(self.session.get,url,*args,**kwargs))
     #     response = await future
     #     print(response.status_code,url)
     #     self.get_comments(response)
 
-    def download(self,url,*args,**kwargs):
-        response = self.session.get(url,*args,**kwargs)
-        print(response.status_code,url)
-        self.get_comments(response)
+    async def fetch(self,session,url):
+        with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(response.status,url)
+            text = await response.text()
+            self.get_comments(text)
 
-    def download_one(self,url):
-        r = self.session.get(url)
-        # tasks = [asyncio.ensure_future(self.download(url+\
-        #         '?start={}&limit=20&sort=new_score&status=P'.format(i*20))) \
-        #         for i in range(10)]
-        # self.loop.run_until_complete(asyncio.gather(*tasks))
-        urls = [url+'?start={}&limit=20&sort=new_score&status=P'.format(i*20) for i in range(10)]
-        pool = Pool(10)
-        pool.map(self.download,urls)
+    async def download(self,loop,urls):
+        async with aiohttp.ClientSession(loop=loop) as session:
+            session.headers = {
+            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch, br",
+            "Accept-Language": "zh-CN, zh; q=0.8, en; q=0.6",
+            }
+            tasks = [asyncio.ensure_future(self.fetch(session,url) for url in urls]
+            htmls = await asyncio.gather(*tasks)
 
 
 
     def crawls(self):
         df = pd.read_csv('moviedata.csv')
-        urls = df['url'].apply(lambda url:url+'comments')
-        pool = Pool(10)
-        pool.map(self.download_one,urls)
-        
-        self.loop.close()
+        df_urls = df['url'].apply(lambda url:url+'comments')
+        urls = []
+        for url in df_urls:
+             tmp = [url+'?start={}&limit=20&sort=new_score&status=P'.format(i*20))) for i in range(10)]
+             urls.extend(tmp)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.download(loop,urls))
+        loop.close()
 
 if __name__ == '__main__':
     cc = CrlComments()
